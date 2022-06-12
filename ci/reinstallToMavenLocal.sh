7eb3d388555f9fa999b6323187332bfd76907b71
bailuk <bailu@bailu.ch>
2022-06-12 18:22:18 +0200

    Local build

---
+++ ci/reinstallToMavenLocal.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# use `make install` instead
+
+test -f gradlew || cd ..
+
+repoDir="${HOME}/.m2/repository/org/mapsforge/mapsforge-map-gtk/"
+
+echo
+echo "Remove 'java-gtk' from local Maven repository"
+rm -r ${repoDir}
+
+echo
+echo "Install 'java-gtk' to local Maven repository"
+./gradlew -q publishToMavenLocal || exit 1
+find ${repoDir}
--- mapsforge-map-gtk/build.gradle.kts
+++ mapsforge-map-gtk/build.gradle.kts
@@ -7,8 +7,30 @@ plugins {
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
 
+fun getProperty(property: String, default: String) : String {
+    var result = default
+
+    if (project.hasProperty(property)) {
+        val r = project.property(property)
+        if (r is String && r != "unspecified") {
+            result = r
+        }
+    }
+    return result
+}
+project.version = getProperty("version", "SNAPSHOT")
+project.group = "org.mapsforge"
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
 
 repositories {
+    mavenLocal()
     mavenCentral()
     repositories {
         maven { url = uri("https://jitpack.io") }
@@ -20,8 +42,8 @@ dependencies {
     val mapsForgeVersion: String by project
     implementation("org.mapsforge:mapsforge-map-reader:$mapsForgeVersion")
 
-    //api 'ch.bailu.java-gtk:java-gtk:0.1-SNAPSHOT'
-    api("com.github.bailuk:java-gtk:0.1")
+    api("ch.bailu.java-gtk:java-gtk:0.1-SNAPSHOT")
+    //api("com.github.bailuk:java-gtk:0.1")
 
     val junitVersion: String by project
     testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
