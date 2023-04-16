39e05cdc5696e721e88a45971b1d79d5def3a4e9
bailuk <bailu@bailu.ch>
2023-04-16 12:59:58 +0200

    Refactor sample

--- mapsforge-samples-gtk/src/main/java/HelloMap.java
+++ mapsforge-samples-gtk/src/main/java/HelloMap.java
@@ -11,16 +11,22 @@ import ch.bailu.gtk.type.Str;
 import ch.bailu.gtk.type.Strs;
 
 public class HelloMap {
+    private final static Str APP_ID = new Str("org.mapsforge.samples.hello_map");
+    private final static Str TITLE = new Str("Hello map");
+
+    private final static String USER_AGENT = "mapsforge-samples-gtk";
+    private final static int TILE_SIZE = 256;
+
     public static void main(String[] args) {
-        final Application app = new Application(new Str("org.mapsforge.samples.hello-map"), ApplicationFlags.FLAGS_NONE);
+        final Application app = new Application(APP_ID, ApplicationFlags.FLAGS_NONE);
 
         app.onActivate(() -> {
-            var window = new ApplicationWindow(app);
-            final MapView mapView = new MapView();
+            final var window = new ApplicationWindow(app);
+            final var mapView = new MapView();
 
             initDownloadTilesMap(mapView);
             mapView.setZoomLevel((byte) 1);
-            window.setTitle("Hello Map");
+            window.setTitle(TITLE);
             window.setSizeRequest(400,400);
             window.setChild(mapView.getDrawingArea());
             window.onDestroy(()->System.exit(0));
@@ -32,18 +38,19 @@ public class HelloMap {
 
 
     private static void initDownloadTilesMap(MapView mapView) {
-        var layers = mapView.getLayerManager().getLayers();
-        var tileSource = OpenStreetMapMapnik.INSTANCE;
-        var tileCache = TileCacheUtil.createTileCache(mapView.getModel());
-        var tileDownloadLayer = new TileDownloadLayer(tileCache, mapView.getModel().mapViewPosition,
+        final var tileSource = OpenStreetMapMapnik.INSTANCE;
+        final var tileCache = TileCacheUtil.createTileCache(mapView.getModel());
+        final var tileDownloadLayer = new TileDownloadLayer(tileCache, mapView.getModel().mapViewPosition,
                 tileSource, GtkGraphicFactory.INSTANCE);
+        final var layers = mapView.getLayerManager().getLayers();
 
-        mapView.getModel().displayModel.setFixedTileSize(256);
+        layers.add(0,tileDownloadLayer);
+
+        mapView.getModel().displayModel.setFixedTileSize(TILE_SIZE);
         mapView.setZoomLevelMin(tileSource.getZoomLevelMin());
         mapView.setZoomLevelMax(tileSource.getZoomLevelMax());
 
-        layers.add(0,tileDownloadLayer);
-        tileSource.setUserAgent("mapsforge-samples-gtk");
+        tileSource.setUserAgent(USER_AGENT);
         tileDownloadLayer.start();
     }
 }
