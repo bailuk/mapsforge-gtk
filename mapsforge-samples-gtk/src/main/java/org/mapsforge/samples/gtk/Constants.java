068c1b029b5fc8ba469d8316d28150754c19da5d
bailuk <bailu@bailu.ch>
2023-04-25 16:35:46 +0200

    Update axon-release plugin

--- mapsforge-samples-gtk/src/main/java/org/mapsforge/samples/gtk/Constants.java
+++ mapsforge-samples-gtk/src/main/java/org/mapsforge/samples/gtk/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public final static Str APP_ID   = new Str("org.mapsforge.samples.gtk");
     public final static Str APP_NAME = new Str("Mapsforge GTK4 Sample");
 
-    public final static Str VERSION_NAME= new Str("v0.2.0");
+    public final static Str VERSION_NAME= new Str("v0.3.0");
     public final static Str WEBSITE = new Str("https://github.com/bailuk/mapsforge-gtk");
 
     public static final Str DIALOG_CLOSE = new Str("Close");
