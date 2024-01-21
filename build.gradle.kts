bff7f8a0fd7abd388c0b94775410aa767aad2e4c
bailuk <bailu@bailu.ch>
2024-01-21 21:45:53 +0100

    Release 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,9 @@ plugins {
     /**
      * https://github.com/allegro/axion-release-plugin
      *
-     * 1. Current build version
+     * Current build version
      * ./gradlew cV
      *
-     * 2. Move to next version
-     * git tag v0.3.0-pre.1
-     * git push origin v0.3.0-pre.1
-     *
-     * 3. Release
-     * -> Update README.md
-     * git tag v0.3.0 "Release"
      */
     id ("pl.allegro.tech.build.axion-release") version "1.15.0"
 }
